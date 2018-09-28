acb3c3b3579c24ef632aca1fe40193ab4f977975
Gilbert Champion <gchampion@liveperson.com>
2018-09-28 14:40:11 -0400

    Install as egg

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires=dependencies,
     entry_points={
         'console_scripts': [
-            'workflow-shell = workflow_shell.cli:main',
+            'wshp = workflow_shell.cli:main',
         ],
     },
     classifiers=[
