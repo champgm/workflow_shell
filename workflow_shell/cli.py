16834a6c8b5ea03b332ac29c3392d4df97ade8eb
mac <github@champgm.e4ward.com>
2018-09-28 23:30:00 +0000

    It kind of works

--- workflow_shell/cli.py
+++ workflow_shell/cli.py
@@ -4,33 +4,33 @@ import importlib
 # importlib.import_module('git.git-status')
 
 package, module = __name__.rsplit('.', 1)
-gs = importlib.import_module(f'{package}.git.git-status')
+gs = importlib.import_module(package + '.git.git-status')
 
 
-@click.command()
+@click.group()
 @click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
-@click.argument('name', default='world', required=False)
-def wshp(name, as_cowboy):
+def wshp(as_cowboy):
     """Workflow Shell"""
     greet = 'Howdy' if as_cowboy else 'Hello'
-    click.echo('{0}, {1}.'.format(greet, name))
+    click.echo('{0}, {1}.'.format(greet, 'name'))
     click.echo(__name__)
     click.echo(gs)
     click.echo(dir(gs))
 
-    gs.main()
-    commandized = commandize(gs.main)
-    setattr(gs, 'main', commandized)
+    # gs.main()
+    # commandized = commandize(gs.main)
+    # setattr(gs, 'main', commandized)
 
 
 def commandize(function):
-    @wshp.command()
+      click.echo(function.__name__)
+
+    @wshp.command(name='gs')
     def wrapper(*args, **kwargs):
         print(function)
         return function(*args, **kwargs)
+    click.echo(wrapper)
     return wrapper
 
+commandize(gs.main)
 
-@wshp.command()
-def asdf():
-    click.echo('asdf')
