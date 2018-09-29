70ad3f7ad9cedc408bf4a72519a45bfa47ad13d9
mac <github@champgm.e4ward.com>
2018-09-29 16:35:08 +0000

    Figured out subcommand help

--- workflow_shell/git/git-status.py
+++ workflow_shell/git/git-status.py
@@ -1,9 +1,16 @@
 import click
 
+command_string = 'gs'
+command_help = 'haplk'
 
 # @wshp.command()
+
+
 def main():
+    """
+    asdf
+    """
     click.echo('gs called')
 
-def get_command_name():
-  return 'gs'
+# def get_command_string():
+    # return 'gs'
