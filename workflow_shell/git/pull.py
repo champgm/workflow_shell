8b95ca5d6c45ac0fa3a47262faf37f3db27f34d1
mac <github@champgm.e4ward.com>
2018-09-30 17:31:56 +0000

    test commit, please fixup

--- workflow_shell/git/pull.py
+++ workflow_shell/git/pull.py
@@ -2,7 +2,7 @@ import subprocess
 import click
 from ..util import get_result
 from ..util import run
-
+from ..cli import all_commands
 
 command_string = 'gp'
 command_help = 'Takes one optional argument, the name of the base branch to pull and rebase with'
@@ -14,6 +14,8 @@ argument_default = 'develop'
 def main(*args, **kwargs):
     # click.echo(str(args))
     # click.echo(str(kwargs))
+    context = args[0]
+    context.invoke(all_commands['gcam'])
     base_branch = kwargs.get(command_arguments[0])
     click.echo('base_branch: '+str(base_branch))
     current_branch = get_result(['git', 'rev-parse', '--abbrev-ref', 'HEAD'])
