ecf626e7057b87be8685d4b8fc650ce463ddaa6e
mac <github@champgm.e4ward.com>
2018-09-30 17:53:01 +0000

    test commit, please fixup

--- workflow_shell/git/force-push.py
+++ workflow_shell/git/force-push.py
@@ -8,7 +8,7 @@ command_string = 'gpfo'
 command_help = 'Force pushes the current branch into a remote branch of the same name'
 
 
-def main():
+def main(*args, **kwargs):
     branch_name = get_result(['git', 'rev-parse', '--abbrev-ref', 'HEAD'])
     if branch_name != 'master' and branch_name != 'develop':
       run(['git', 'push', '-f', 'origin', branch_name])
--- workflow_shell/git/status.py
+++ workflow_shell/git/status.py
@@ -5,5 +5,5 @@ command_string = 'gs'
 command_help = 'Displays the current status of the git repository'
 
 
-def main():
+def main(*args, **kwargs):
     run(['git', 'status'])
