f46912c9dd43cb38dc40d0c376eb4aea222430e2
Gilbert Champion <gchampion@liveperson.com>
2019-02-26 11:06:33 -0500

    Add clipboard timestamp and parameter URL commands

--- workflow_shell/clipboard/timestamp.py
+++ workflow_shell/clipboard/timestamp.py
@@ -10,5 +10,5 @@ short_help = "Clipboard - Puts a timestamp onto the clipboard"
 
 def main(*args, **kwargs):
     timestamp = time.time()
-    formatted = datetime.datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
-    pyperclip.copy(formatted)
+    formatted = datetime.datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M")
+    pyperclip.copy(formatted + " ")
