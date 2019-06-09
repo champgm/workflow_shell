df6c75fad5f1b8771f1be115b2112fd1c2801389
champgm <github@champgm.e4ward.com>
2019-06-09 14:48:04 -0400

    Got gpfo working

--- src/workflow_shell/commands/git/force_push.clj
+++ src/workflow_shell/commands/git/force_push.clj
@@ -5,8 +5,9 @@
 (def gpfo (hash-map
            :description "Force pushes the current branch into a remote branch of the same name"
            :execute (fn [& args]
-                      (let [branch-name (:out (sh "git" "rev-parse" "--abbrev-ref" "HEAD"))
+                      (let [branch-name (clojure.string/trim (:out (sh "git" "rev-parse" "--abbrev-ref" "HEAD")))
                             result  (sh "git" "push" "-f" "origin" branch-name)]
+                        (println result)
                         (if-not (empty? (:out result))
                           (println (:out result))
                           (println (:err result)))))))
